1a5d3f7c2e2cd00ca73b5a140b74357b5c106a11
Chintan <cvora22@gmail.com>
2016-04-04 19:43:51 -0700

    Chapter 1 Project

---
+++ chapter1/src/main/java/app/free/chapter1/HelloWorld.java
@@ -0,0 +1,13 @@
+package app.free.chapter1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloWorld extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+    }
+}
